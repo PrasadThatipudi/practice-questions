613b65a98ff14b54786bb282491c0b28df01b575
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2024-12-22 11:50:43 +0530

    adds filterEvenNumbers()

--- filter.js
+++ filter.js
@@ -1,5 +1,13 @@
 // even numbers [1, 2, 3, 4, 5] => [2, 4]
-const filterEvenNumbers = function (numbers) { };
+const isEven = function (number) {
+  return (number & 1) === 0;
+};
+
+const filterEvenNumbers = function (numbers) {
+  return numbers.filter(isEven);
+};
+
+console.log(filterEvenNumbers([1, 2, 3, 4]));
 
 // words with more than 5 letters ["apple", "banana", "kiwi", "grape"] => ["banana"]
 const filterLongWords = function (words) { };
